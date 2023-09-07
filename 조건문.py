b256cd6e1534b58d9adc055f605389597fd2851d
jojo6807 <jyj05181@gmail.com>
2023-09-07 11:25:05 +0900

    조건문

---
+++ 조건문.py
@@ -0,0 +1,27 @@
+# 제어문이란 조건문과 반복문을 의미하며 순차적인 흐름을 제어하는 목적으로 사용 된다.
+# n = int(input("정수 입력 :"))
+# if n > 100:
+#     print(f"{n}은 100보다 큼")
+# elif n < 100:
+#     print(f"{n}은 100보다 작음")
+# else: print(f"{n}은 100과 같음") # 한줄로 써도 오류는 안남
+
+# 조건문에서 문자열 비교
+# while True :
+#     season = input("계절을 입력 : ")
+#     if season == "spring":
+#     # 자바에서는 eaqualsIgnores()로 비교하거나 스위치문으로 비교해야하지만(왜냐하면 자바는 그냥 비교해버리면 주소를 비교하는 꼴이다.),
+#     # 파이썬은 그냥 비교해도 됨
+#         print("봄")
+#     elif season == "summer":
+#         print("여름")
+#     elif season == "fall" or season == "autumn":
+#         print('가을')
+#     elif season == "winter":
+#         print("겨울")
+#     else: # 파이썬은 else가 있어야 종료가 됨.
+#         pass
+#         break
+
+
+
