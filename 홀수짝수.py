dc60256190a12e4bff91a429bebab8e2745e498d
jojo6807 <jyj05181@gmail.com>
2023-09-08 17:25:03 +0900

    홀짝

--- 홀수짝수.py
+++ 홀수짝수.py
@@ -1,3 +1,7 @@
+# map : 전달 받은 값을 함수내부에서 가공해서 반환한다. 입력 개수와 반환 개수가 동일
+# filter : 전단 받은 값을 함수내부에서 조건에 일치하는 것만 골라서 반환한다.
+
+
 arr = list(map(int, input().split()))
 eve = []
 odd = []
@@ -10,7 +14,7 @@ for e in arr:
         odd.append(e)
 print(eve)
 print(odd)
-
+#filter
 eve2 = list(filter(lambda e : int(e)% 2 == 0, arr))
 odd2 = list(filter(lambda e : int(e)% 2 != 0, arr))
 print(eve2)
