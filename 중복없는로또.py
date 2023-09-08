1b3a12d80a58caccabdac4e5441c86b2e60f661b
jojo6807 <jyj05181@gmail.com>
2023-09-08 12:32:46 +0900

    중복없는 로또

--- 중복없는로또.py
+++ 중복없는로또.py
@@ -5,7 +5,7 @@ print("로또")
 ls = [] # 빈리스트 만들기
 while True :
     rand = random.randrange(1, 46) # 46미만
-    if rand not in ls :
-        ls.append(rand)
+    if rand not in ls : # 생성된  list에 rand값이 없으면 
+        ls.append(rand) # append로 rand 추가
     if len(ls) == 6 : break
 print(ls)
