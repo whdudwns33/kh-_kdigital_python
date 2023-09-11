fa838419b8745233998e05f31eaf76f91a208ac4
jojo6807 <jyj05181@gmail.com>
2023-09-11 16:37:09 +0900

    함수와 람다 함수

---
+++ 람다함수.py
@@ -0,0 +1,45 @@
+# 람다란 간단한 함수의 선언과 호출을 하나의 식으로 간략히 표현한 것입니다.
+# 파이썬에서 람다 함수를 통해 이름이 없는 함수를 만들 수 있습니다.
+# 람다 함수의 장점은 코드의 간결함, 메모리 절약이라고 생각할 수 있습니다.
+
+
+# def add(a, b):
+#     return a + b
+# print(add(1,2))
+# print((lambda a, b : a + b)(1,2))
+
+
+# 함수의 매개변수를 함수로 전달
+# def call_times(func) :  # 매개변수에 함수를 선언할 수 있음
+#     for i in range (10) :   # 10번 반복
+#         func()              # 매개변수의 함수를 10번 반복
+#
+# def print_hello() :
+#     print(f"hello ")
+#
+# call_times(print_hello)   # 함수에 매개변수를 함수로 선언
+
+
+# filter() map()
+# filter(함수, 리스트): 리스트의 요소를 함수에 넣고 리턴값이 True인 것으로 새로운 리스트를 구성
+# map(함수, 리스트): 리스트의 요소를 함수에 넣고 새로운 리스트로 구성
+
+#power 함수
+def power(n) :
+    return n * n
+#람다로 선언한 power
+power_lambda = lambda n: n * n
+
+in_ = [1,2,3,4,5]
+out_1 = list(map(power, in_))
+out_2 = list(map(power_lambda, in_))
+out_3 = list(map(lambda n: n * n, in_))
+
+print(out_1)
+print(out_2)
+print(out_3)
+
+
+
+
+
--- 함수.py
+++ 함수.py
@@ -2,19 +2,77 @@
 
 
 # 이름, 주소, 전화번호를 매개변수로 전달받아 출력하는 함수
-def name_card(name, addr, phone):
-    print(f"\n"
-          f"주소 : {addr} \n"
-          f"번호 : {phone} \n"
-          f"이름 : {name} \n"
-          f"대륙 : VI")   # 회사 부분은 공통점
+# def name_card(name, addr, phone):
+#     print(f"\n"
+#           f"주소 : {addr} \n"
+#           f"번호 : {phone} \n"
+#           f"이름 : {name} \n"
+#           f"대륙 : VI")   # 회사 부분은 공통점
+#
+# # 함수는 선언 이후 호출 해야만 동작을 한다.
+# # 한번 만들어진 함수를 여러번 호출하여 반복 수행이 가능함. 또한 반복 수행하면서 코드를 줄일 수 있음.
+# name_card("전사","페리온","001")
+# name_card("궁수","헤네시스","002")
+# name_card("마법사","엘레니아","003")
+# name_card("도적","커닝시티","004")
+#
+#
+# # 순차검색
+# def search_list(a,x):
+#     for i in range(len(a)):
+#         if x == a[i]:
+#             return i
+#         else:
+#             return -1
+#
+# v = [17,18,19,92,33,58,7]
+# print(search_list(v, 33))
+# print(search_list(v, 18))
+# print(search_list(v, 7))
+# print(search_list(v, 100))
+#
+# # 기본값 인자: 함수 선언 시 매개변수에 대해서 기본값을 정의 할 수 있습니다.
+# # 매개변수에 기본값이 정의 되어 있는 경우 함수호출 시 인자값을 넣지 않으면 기본값을 호출 함.
+# def pofile(name, year = 2, age = 18, school = "태양고"):# name은 default값이 없으니까 무조건 입력해야함. 나머지는 defauit값이 있음
+#     print(f"이름 : {name}, 학교 : {school}, 학년{year}, 나이 : {age}")
+#
+# pofile("나희도")
+# pofile("고유림")
+# pofile("백이진", "졸업", 22, "태양고 출신")
 
-# 함수는 선언 이후 호출 해야만 동작을 한다.
-# 한번 만들어진 함수를 여러번 호출하여 반복 수행이 가능함. 또한 반복 수행하면서 코드를 줄일 수 있음.
-name_card("전사","페리온","001")
-name_card("궁수","헤네시스","002")
-name_card("마법사","엘레니아","003")
-name_card("도적","커닝시티","004")
+# 가변 매개변수 : 변수의 개수가 정해지지 않은 경우에 사용.
+# *를 붙여 사용. 함수의 매개 변수를 몇개 입력하든 함수내에서 튜플로 인식. 패킹해서 입력됨
+
+# def profile(name , age, *lang):
+#     print(f"이름 : {name} 나이 : {age}", end= " ")
+#     for e in lang :
+#         print(e, end= ' ')
+#     print()
+#
+# profile("나희도", 18, "고등학교", "자바", "파이썬", "C", "React")
+# profile("유진초이", 28, "미군", "자바", "파이썬", "C++", "R")
+# profile("정만월", 38, "델루나", "자바")
+
+
+#지역 변수, 전역 변수
+# sword = 10
+# def game(player, sword):
+#     sword -= player # 매개변수로 선언된 sword사용
+#     print(f"남은 검 :{sword}")
+#     return sword
+#
+# def game1(player):
+#     global sword    # 전역에서 선언된 sword를 사용
+#     sword -= player
+#     print(f"남은 검 :{sword}")
+#
+#
+# player = int(input("경기에 참여하는 노예가 몇명인가. :"))
+# sword = game(player, sword)
+# sword = game1(player)
+# print(f"경기에 사용하고 남은 검 : {sword}")
+
+
 
 
 
